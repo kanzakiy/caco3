6d8f322bd59b3cb61e058fa71811358d6f056d13
kanzakiy <kanzakiy@ucr.edu>
2019-03-30 19:09:04 -0700

    list of defined macros

---
+++ defines.h
@@ -0,0 +1,38 @@
+! using 'test' directory 
+#define test 
+
+! testing 5kyr signal change event 
+!#define biotest
+
+! testting two size caco3 simulation 
+!#define size 
+
+! without signal tracking 
+!#define sense
+
+! using method2 to track signals (default 42 species)
+!#define track2 
+
+! not showing results on display 
+!#define nondisp
+
+! not recording profiles 
+!#define nonrec
+
+! shwoing every iteration 
+!#define showiter
+
+! using sparse matrix solve (you need UMFPACK) 
+#define sparse
+
+! all turbo2 mixing 
+!#define turbo2 
+
+! all labs mixing 
+!#define labs 
+
+! no bioturbation 
+#define nobio 
+
+! enabling only oxic degradation of om 
+!#define oxonly
