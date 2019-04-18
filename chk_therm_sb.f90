96ca6dd5475d5d94d76d08a8378837591a3583c4
kanzakiy <kanzakiy@ucr.edu>
2019-04-17 21:34:00 -0700

    to chk sbs in caco3_therm (more comments)

--- chk_therm_sb.f90
+++ chk_therm_sb.f90
@@ -19,17 +19,19 @@ sal = 35d0 ! salinity wt o/oo
 dep = 4.0d0 ! depth in km
 dic = 2211d0*1d-6/1d3  ! 2211 uM converted to mol/cm3; all the same value at different grids
 alk = 2285d0*1d-6/1d3
+! calling subroutine to calculate all aqueous co2 species and pH
 call calcspecies(dic,alk,temp,sal,dep,ph,co2,hco3,co3,nz,info)
 if (info/=0) then ! if ph cannot be calculated in the subroutine, info=1 is returned 
     print*, 'error in calcspecies'
     stop
 endif 
+! calling subroutine to calculate derivatives of co3 conc. wrt dic and alk (defined as dco3_ddic and dco3_dalk, respectively)
 call calcdevs(dic,alk,temp,sal,dep,nz,info,dco3_dalk,dco3_ddic)
 if (info/=0) then ! if ph cannot be calculated in the subroutine, info=1 is returned 
     print*, 'error in calcdevs'
     stop
 endif 
-print*,'dep, keqcc, keqag'
+! printing results on screen; if you want check with MATLAB version by copy and paste
 do iz=1,nz
     print*,dic(iz),alk(iz),co2(iz),hco3(iz),co3(iz),ph(iz),dco3_ddic(iz),dco3_dalk(iz)
     ! note that the above concentrations (alk,dic,co2,hco3,co3) are all in mol/cm3
