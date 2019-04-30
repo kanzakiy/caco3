5121da15892df665f2ac6d0c9512211399efb453
kanzakiy <kanzakiy@ucr.edu>
2019-04-30 10:52:02 -0700

    adding macros to use mocsy

--- defines.h
+++ defines.h
@@ -1,20 +1,20 @@
 ! using 'test' directory 
-#define test 
+! #define test 
 
 ! testing 5kyr signal change event 
-!#define biotest
+! #define biotest
 
 ! testting two size caco3 simulation 
 !#define size 
 
 ! without signal tracking 
-!#define sense
+#define sense
 
 ! using method2 to track signals (default 42 species)
 ! #define track2 
 
 ! not showing results on display 
-!#define nondisp
+#define nondisp
 
 ! not recording profiles 
 !#define nonrec
@@ -26,16 +26,28 @@
 #define sparse
 
 ! all turbo2 mixing 
-!#define allturbo2 
+! #define allturbo2 
 
 ! all labs mixing 
-!#define alllabs 
+! #define alllabs 
 
 ! no bioturbation 
 ! #define allnobio 
 
 ! enabling only oxic degradation of om 
-!#define oxonly
+#define oxonly
 
 ! recording the grid to be used for making transition matrix in LABS 
 !#define recgrid
+
+! if assuming no caco3 dissolution 
+! #define nodissolve
+
+! allowing full 2 substitution including 17o
+! #define fullclump
+
+! using mocsy for caco3 thermodynamics 
+! #define mocsy
+
+! digit used for mocsy
+! #define USE_PRECISION=2
