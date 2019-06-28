ae09cc4d3c1aaae6d203972ded67290c4ee1c1ae
kanzakiy <kanzakiy@ucr.edu>
2019-06-28 13:08:00 -0700

    making corrections

--- caco3_sense.py
+++ caco3_sense.py
@@ -6,17 +6,17 @@ if not crntdir in sys.path:sys.path.append(crntdir)
 from caco3 import caco3_main
 import numpy as np
 
-ccflxi      = 12e-6      # CaCO3 rain flux in mol cm-2 yr-1  
-om2cc       = 0.7        # OM/CaCO3 rain ratio 
-dep         = 3.5        # water depth in km  
-dt          = 1e8        # time step in yr  
-fl          = 'sense'    # simulation name 
-biot        = 'fickian'  # bioturbation style  
-oxonly      = False      # Oxic only for OM degradation?  
-runmode     = 'sense'    # simulation mode
-co2chem     = 'co2'      # co2 chemistry
-sparse      = True       # use sparse matrix solver?
-showiter    = False      # show every iteration step?
+ccflxi      = 12e-6         # CaCO3 rain flux in mol cm-2 yr-1  
+om2cc       = 0.7           # OM/CaCO3 rain ratio 
+dep         = 3.5           # water depth in km  
+dt          = 1e8           # time step in yr  
+fl          = 'sense'       # simulation name 
+biot        = 'fickian'     # bioturbation style  
+oxonly      = False         # Oxic only for OM degradation?  
+runmode     = 'sense'       # simulation mode
+co2chem     = 'co2'         # co2 chemistry
+sparse      = False         # use sparse matrix solver?
+showiter    = False         # show every iteration step?
 rrlist      = np.array([0.0,0.5,0.6666,1.0,1.5])
 nz          = 25
 for i in range(nz):
